ec5735fce57c258da9811d9a4a1905dcde824711
eadgbe <theoldmaninthecave@gmail.com>
2016-02-01 10:18:57 +1300

    Update default.rb

--- rails_server/attributes/default.rb
+++ rails_server/attributes/default.rb
@@ -1,1 +1,9 @@
 default['postgresql']['config']['shared_buffers'] = '24MB'
+
+default['postgresql']['pg_hba'] = [
+  {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident'},
+  {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident'},
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'trust'},
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'}
+]
