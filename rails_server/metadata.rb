a17b3237d98b825b208a2609a7fbb94959f9bf91
eadgbe <theoldmaninthecave@gmail.com>
2016-02-01 00:47:19 +1300

    Update metadata.rb

--- rails_server/metadata.rb
+++ rails_server/metadata.rb
@@ -6,3 +6,4 @@ description      'Installs/Configures rails_server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'postgresql', '~> 4.0.0'
